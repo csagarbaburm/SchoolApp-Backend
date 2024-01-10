266f959085b20fbf52a8e1b65a264e182bc46f1d
csagarbaburm <csagarba@rmplc.net>
2024-01-10 10:58:11 +0530

    Revert "Revert "push 0.1""
    This reverts commit a079e7a26c3ff1831578514c69b8ce6032565c7f.

---
+++ ModelValidationAssignment/Interfaces/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using ModelValidationAssignment.Models;
+namespace ModelValidationAssignment.Interfaces
+{
+    public interface IMovieRepository
+    {
+        void AddMovie(Movie movie);
+        Movie GetMovie(string name);
+        List<Movie> GetAllMovies();
+        List<Movie> GetMoviesByActor(string actor);
+        List<Movie> GetMoviesByDirector(string director);
+        List<Movie> GetMoviesByYear(int year);
+    }
+}
